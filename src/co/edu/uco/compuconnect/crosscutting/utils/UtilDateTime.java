9d25e25f3c576d645f793cd79be4998152ac212b
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-04-30 15:26:30 -0500

    resolver conflicto

--- src/co/edu/uco/compuconnect/crosscutting/utils/UtilDateTime.java
+++ src/co/edu/uco/compuconnect/crosscutting/utils/UtilDateTime.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.UUID;
 
 public final class UtilDateTime {
 	
